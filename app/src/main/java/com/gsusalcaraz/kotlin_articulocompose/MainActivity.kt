30f5094f64c70da9df4741bb528a2447d4cbc76f
Jesus Alcaraz <gsus.alcaraz@gmail.com>
2023-06-26 16:09:44 +0200

    Valor de texto por strings.xml

--- app/src/main/java/com/gsusalcaraz/kotlin_articulocompose/MainActivity.kt
+++ app/src/main/java/com/gsusalcaraz/kotlin_articulocompose/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,21 +48,21 @@ fun Articulo(modifier: Modifier = Modifier) {
         )
 
         Text (
-            text = "Jetpack Compose tutorial",
+            text = stringResource(R.string.Titulo),
             textAlign = TextAlign.Justify,
             fontSize = 24.sp,
             modifier = Modifier
                 .padding(16.dp)
         )
         Text(
-            text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            text = stringResource(R.string.Texto),
             textAlign = TextAlign.Justify,
             fontSize = 16.sp,
             modifier = Modifier
                 .padding(16.dp)
         )
         Text(
-            text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+            text = stringResource(R.string.Texto2),
             textAlign = TextAlign.Justify,
             fontSize = 16.sp,
             modifier = Modifier
